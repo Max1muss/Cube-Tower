54cf94f96bd1e6ade0f421b5cf828c6579b425d6
Max1muss <96428071+Max1muss@users.noreply.github.com>
2022-10-24 14:47:31 +0300

    Add files via upload

---
+++ CameraRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform m_rotator;
+
+    private void Start()
+    {
+        m_rotator = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        m_rotator.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
---
+++ CanvasButtons.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasButtons : MonoBehaviour
+{
+    public Sprite musicOn, musicOff;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetString("music") == "No" && gameObject.name == "Music")
+        GetComponent<Image>().sprite = musicOff;
+    }
+
+    public void RestartGame()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+        SceneManager.LoadScene("Main");
+    }
+
+
+    public void LoadShop()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void CloseShop()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+
+        SceneManager.LoadScene("Main");
+    }
+
+    public void LoadInstagram()
+    {
+        if (PlayerPrefs.GetString("music") != "No")
+            GetComponent<AudioSource>().Play();
+        Application.OpenURL("https://www.instagram.com/_.maxximillian._/");
+    }
+
+    public void MusicWork()
+    {
+        if (PlayerPrefs.GetString("music") == "No")
+        {
+            PlayerPrefs.SetString("music", "Yes");
+            GetComponent<Image>().sprite = musicOn;
+            GetComponent<AudioSource>().Play();
+        }
+        else
+        {
+            PlayerPrefs.SetString("music", "No");
+            GetComponent<Image>().sprite = musicOff;
+        }
+    }
+}
